181245e580ce388d4582b0b61fb981e0d9ab6afc
Alexander Nakamura <nakamale@students.zhaw.ch>
2018-03-07 17:37:38 +0100

    search and reset button done, no api yet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,92 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import JobForm from './JobForm';
+import SearchList from './SearchList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Github Jobs</h1>
-        </header>
+    constructor(props) {
+        super(props)
+        this.state = {
+            job: '',
+            location: '',
+            showSearchList: false
+        };
+    }
 
-        <JobForm />
-      </div>
-    );
-  }
+    handleClick(i) {
+        console.log(i.toString())
+        switch (i.toString()) {
+            case 'search':
+                this.onSearch();
+                break;
+            case 'reset':
+                this.onReset();
+                break;
+            case 'detailView':
+                this.onDetailView();
+                break;
+            default:
+                console.log('click not handeled!')
+        }
+    }
+
+    onChange(e) {
+        console.log('onChange called')
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    };
+
+
+    onReset() {
+        this.setState({
+            job: '',
+            location: '',
+            showSearchList: false
+        });
+    };
+
+    onSearch() {
+        const state = this.state;
+        state.showSearchList = true;
+        this.setState(state);
+    };
+
+    onDetailView() {
+        console.log('detail view');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Github Jobs</h1>
+                </header>
+
+                <JobForm
+                    job={this.state.job}
+                    location={this.state.location}
+                    onChange={(e) => this.onChange(e)}
+                    onClick={(i) => this.handleClick(i)}/>
+
+                {/* SearchList component */}
+                {this.state.showSearchList ?
+                    <SearchList
+                    job={this.state.job}
+                    location={this.state.location}
+                    onClick={(i) => this.handleClick(i)}/> :
+                    null
+                }
+
+                <footer>
+                    <h3>current State:</h3>
+                    <p>{"->" + this.state.job.toString()}</p>
+                    <p>{"->" + this.state.location.toString()}</p>
+                    <p>{"->" + this.state.showSearchList.toString()}</p>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/JobForm.js
+++ src/JobForm.js
@@ -1,64 +1,19 @@
 import React, {Component} from 'react';
 import './JobForm.css';
-import SearchList from "./SearchList";
 
 
 class JobForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            job: '',
-            location: '',
-            showSearchList: false
-        };
-        this.onSearch = this.onSearch.bind(this);
-        this.onReset = this.onReset.bind(this);
-    };
-
-    onChange = (e) => {
-        console.log('onChange called')
-        const state = this.state
-        state[e.target.name] = e.target.value;
-        this.setState(state);
-    };
-
-    onReset() {
-        this.setState({
-            job: '',
-            location: '',
-            showSearchList: false
-        });
-    }
-
-    onSearch() {
-        const state = this.state;
-        state.showSearchList = true;
-        this.setState(state);
-    };
 
     render() {
-        const {job, location} = this.state;
         return (
             <div>
                 <div className="JobForm">
-                    <input type="text" name="job" value={job} onChange={this.onChange} placeholder="Job"/>
-                    <input type="text" name="location" value={location} onChange={this.onChange}
+                    <input type="text" name="job" value={this.props.job} onChange={this.props.onChange} placeholder="Job"/>
+                    <input type="text" name="location" value={this.props.location} onChange={this.props.onChange}
                            placeholder="Location"/>
                     <br/>
-                    <button onClick={this.onSearch}>Search</button>
-                    <button onClick={this.onReset}>Reset</button>
-                </div>
-                <div>
-                    <h3>current State:</h3>
-                    <p>{"->" + this.state.job.toString()}</p>
-                    <p>{"->" + this.state.location.toString()}</p>
-                    <p>{"->" + this.state.showSearchList.toString()}</p>
-                </div>
-                <div>
-                    {this.state.showSearchList ?
-                        <SearchList/> :
-                        null
-                    }
+                    <button onClick={() => this.props.onClick('search')}>Search</button>
+                    <button onClick={() => this.props.onClick('reset')}>Reset</button>
                 </div>
             </div>
         );
