fbd75080a90bc96374b64963d51d227901ef76dd
Steven Imhof <steven.imhof@hotmail.ch>
2018-03-13 13:51:55 +0100

    bugfix

--- src/Details.js
+++ src/Details.js
@@ -10,7 +10,6 @@ class Details extends Component {
     render() {
         const job = this.props.job[0];
         return (
-            <!-- TBD css for content -->
             <div id="content" className="detail-view">
                 <button onClick={() => this.props.onClick('back')}>Back</button>
                 <h1>Details:</h1>
@@ -18,7 +17,7 @@ class Details extends Component {
                 Location: {job.location}<br/>
                 Company: {job.company}<br/>
                 Type: {job.type}<br/>
-                <!-- TBD onerror, alternative placeholder img not working at the moment -->
+                { /* TBD onerror, alternative placeholder img not working at the moment */ }
                 <img src={job.company_logo} onerror="this.src='http://mumlovestech.com/wp-content/uploads/2013/07/git-hub.png';"/>
                 <h3>Description:</h3>
                 <div dangerouslySetInnerHTML={{__html: job.description}} />
@@ -27,9 +26,5 @@ class Details extends Component {
         );
     }
 }
-<div id="content">
-    <img src="images/ribbon.png" class="ribbon"/>
-    <div>some text...</div>
-</div>
 
 export default Details;
